9f3f5c973a2d13fbc8d6ff69ece6d1417f9f86d0
Jose <80800807+vikiii22@users.noreply.github.com>
2021-05-16 12:07:58 +0200

    Dando diseño

---
+++ Otra-Pagina/paginacion.js
@@ -0,0 +1,31 @@
+const sr = ScrollReveal({
+    duration: 3000
+});
+
+sr.reveal('header', {
+    origin: 'left',
+    distance: '1000px',
+    scale: 0.5,
+    delay: 1000,
+});
+
+sr.reveal('body', {
+    origin: 'top',
+    distance: '1000px',
+});
+
+sr.reveal('footer', {
+    origin: 'left',
+    distance: '1000px',
+    reset: true
+});
+
+sr.reveal('.card', {
+    origin: 'left',
+    reset: true
+}, 500);
+
+sr.reveal('h2', {
+    origin: 'left',
+    reset: true
+}, 500);
